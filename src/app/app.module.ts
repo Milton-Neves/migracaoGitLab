babe20b0d175c45e867f09a797bde1367fc205ee
Milton-Neves <milzonzneves@gmail.com>
2022-02-10 17:33:37 -0300

    Feat: Criar componente Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { FooterComponent } from './core/components/footer/footer.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
