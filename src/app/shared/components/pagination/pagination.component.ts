1064de26275cac3bdf3d8f01d1087b899e989bdf
Milton <milzonzneves@gmail.com>
2022-02-14 22:09:17 -0300

    feat: :sparkles: Paginação

---
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss']
+})
+export class PaginationComponent implements OnInit {
+
+  pageAtual = 1;
+  numPaginas = 5;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
