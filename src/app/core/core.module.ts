b9f17fc3b7913e0bd5773128278ce73fd102a3a0
Lauralisse <lauralisseoliveira@outlook.com>
2022-02-15 11:21:56 -0300

    feat: criar sidebar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { SidebarComponent } from './components/sidebar/SidebarComponent'
 
 @NgModule({
-  declarations: [],
+  declarations: [SidebarComponent],
   imports: [CommonModule],
 })
 export class CoreModule {}
