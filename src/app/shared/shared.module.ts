d96a8051bc6b500aea4a330b8b1e9ef349623ce4
Milton Bezerra das  Neves Neto <milton@parnamirim.rn>
2022-02-16 11:55:28 -0300

    feat: branch em produçao

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { PaginationComponent } from './components/pagination/pagination.component'
+import { ButtonComponent } from './components/button/button.component'
 
 @NgModule({
-  declarations: [PaginationComponent],
+  declarations: [PaginationComponent, ButtonComponent],
   imports: [CommonModule],
 })
 export class SharedModule {}
