df1dddb9e295193bd8fc041a932aadd7ce9e9802
BiAaa <anny.ribeiro@parnamirim.rn.gov.br>
2022-02-16 13:27:13 -0300

    feat: posicionamento de elementos na tela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { ManagerModule } from './manager/manager.module'
+import { LoginModule } from './login/login.module'
+import { LoginComponent } from './login/login.component'
+import { LayoutComponent } from './core/components/layout/layout.component'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,9 +4,17 @@ import { FooterComponent } from './components/footer/footer.component'
 import { LogoutComponent } from './components/logout/logout.component'
 import { SidebarComponent } from './components/sidebar/SidebarComponent'
 import { RouterModule } from '@angular/router'
+import { LayoutComponent } from './components/layout/layout.component'
+import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
-  declarations: [FooterComponent, LogoutComponent, SidebarComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [
+    FooterComponent,
+    LogoutComponent,
+    SidebarComponent,
+    LayoutComponent,
+  ],
+  imports: [CommonModule, RouterModule, SharedModule],
+  exports: [LayoutComponent],
 })
 export class CoreModule {}
