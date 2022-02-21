c005f3f8ae6eb218baa656df6b23ca4816047e52
Igor <igorj000@gmail.com>
2022-02-21 11:42:15 -0300

    feat: criando pagina de listagem de curriculo

--- src/app/manager/manager-routing.module.ts
+++ src/app/manager/manager-routing.module.ts
@@ -12,6 +12,11 @@ const routes: Routes = [
         loadChildren: () =>
           import('./home/home.module').then((m) => m.HomeModule),
       },
+      {
+        path: 'curriculos',
+        loadChildren: () =>
+          import('./resume/resume.module').then((m) => m.ResumeModule),
+      },
     ],
   },
 ]
