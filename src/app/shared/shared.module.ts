f64e53b8721c1182f7227c832b50a0a534961457
Igor <igorj000@gmail.com>
2022-02-21 09:57:40 -0300

    refactor: ajustando cores do Pagination Number

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common'
 import { PaginationComponent } from './components/pagination/pagination.component'
 import { InformationCardComponent } from './components/information-card/information-card.component'
 import { TableComponent } from './components/table/table.component'
+import { PaginationNumberComponent } from './components/pagination-number/pagination-number.component'
 
 const components = [
   PaginationComponent,
   PageTitleComponent,
   InformationCardComponent,
-  PaginationComponent,
   TableComponent,
+  PaginationNumberComponent,
 ]
 
 @NgModule({
