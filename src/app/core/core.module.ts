88e9ec75944ea4c1eb38b66339bfafc3ed805022
Milton Bezerra das  Neves Neto <milton@parnamirim.rn>
2022-02-21 10:05:25 -0300

    feat: modificaçao module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FooterComponent } from './components/footer/footer.component'
 import { LogoutComponent } from './components/logout/logout.component'
-import { SidebarComponent } from './components/sidebar/SidebarComponent'
 import { RouterModule } from '@angular/router'
+import { LayoutComponent } from './components/layout/layout.component'
+import { SharedModule } from '../shared/shared.module'
+import { SidebarComponent } from './components/sidebar/sidebar.component'
 
 @NgModule({
-  declarations: [FooterComponent, LogoutComponent, SidebarComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [
+    FooterComponent,
+    LogoutComponent,
+    LayoutComponent,
+    SidebarComponent,
+  ],
+  imports: [CommonModule, RouterModule, SharedModule],
+  exports: [LayoutComponent],
 })
 export class CoreModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
+import { PageTitleComponent } from './components/page-title/page-title.component'
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { PaginationComponent } from './components/pagination/pagination.component'
 import { ButtonComponent } from './components/button/button.component'
 
 @NgModule({
-  declarations: [PaginationComponent, ButtonComponent],
+  declarations: [PaginationComponent, PageTitleComponent],
   imports: [CommonModule],
+  exports: [PageTitleComponent],
 })
 export class SharedModule {}
