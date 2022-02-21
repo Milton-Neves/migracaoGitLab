77ee693f8af0c6c3e6318c215a438495c821d5a4
Igor <igorj000@gmail.com>
2022-02-21 09:50:38 -0300

    fix: resolvendo conflito e removendo codigos desnecessarios

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,20 @@ import { PageTitleComponent } from './components/page-title/page-title.component
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { PaginationComponent } from './components/pagination/pagination.component'
+import { InformationCardComponent } from './components/information-card/information-card.component'
+import { TableComponent } from './components/table/table.component'
+
+const components = [
+  PaginationComponent,
+  PageTitleComponent,
+  InformationCardComponent,
+  PaginationComponent,
+  TableComponent,
+]
 
 @NgModule({
-  declarations: [PaginationComponent, PageTitleComponent],
+  declarations: components,
   imports: [CommonModule],
-  exports: [PageTitleComponent],
+  exports: components,
 })
 export class SharedModule {}
