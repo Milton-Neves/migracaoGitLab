d79df1e552498e014a3a871433944758d0908f62
Igor <igorj000@gmail.com>
2022-02-22 13:28:26 -0300

    feat: add lib de modal

---
+++ src/lib/ngx-modal/src/lib/ngx-modal.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core'
+
+import { NgxModalComponent } from './ngx-modal.component'
+
+@NgModule({
+  declarations: [NgxModalComponent],
+  imports: [],
+  exports: [NgxModalComponent],
+})
+export class NgxModalModule {}
---
+++ src/lib/ngx-modal/src/lib/ngx-modal.service.ts
@@ -0,0 +1,64 @@
+import { ApplicationRef, ComponentRef, Injectable, Type } from '@angular/core'
+import { Observable } from 'rxjs'
+
+import { NgxModalComponent } from './ngx-modal.component'
+
+interface Options {
+  ignoreBackClick?: boolean
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class NgxModalService {
+  private modals: ComponentRef<any>[] = []
+
+  id?: string
+  constructor(private appRef: ApplicationRef) {}
+
+  add(modal: ComponentRef<NgxModalComponent<any>>) {
+    this.modals.push(modal)
+  }
+
+  open<T>(
+    component: Type<T>,
+    data?: Partial<T>,
+    options?: Options
+  ): Observable<NgxModalComponent<T>> {
+    return new Observable<NgxModalComponent<T>>((subscriber) => {
+      const root = document.querySelector('app-root')
+
+      const modalDiv = document.createElement('div')
+      modalDiv.id = 'modal-injector-' + this.modals.length
+      root?.appendChild(modalDiv)
+
+      const modalRef: ComponentRef<NgxModalComponent<T>> =
+        this.appRef.bootstrap(
+          NgxModalComponent,
+          `#${modalDiv.id}`
+        ) as ComponentRef<NgxModalComponent<T>>
+      modalRef.instance.open(component, data, options)
+      this.add(modalRef)
+
+      subscriber.next(modalRef.instance)
+    })
+  }
+
+  close(component?: ComponentRef<any>) {
+    if (component !== undefined) {
+      for (let i = this.modals.length - 1; i >= 0; i--) {
+        const refModal = this.modals[i]
+        if (
+          component.componentType.name ===
+          refModal.instance.childComponentRef.componentType.name
+        ) {
+          refModal.instance.close()
+          refModal.destroy()
+          break
+        }
+      }
+      return
+    }
+    this.modals[this.modals.length - 1].instance.close()
+    this.modals[this.modals.length - 1].destroy()
+  }
+}
