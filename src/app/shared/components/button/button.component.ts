548b6e4e565a64daba6858de7b4cae6cb7b1a08e
Milton Bezerra das  Neves Neto <milton@parnamirim.rn>
2022-02-22 11:56:08 -0300

    feat: configurando components pai e filho

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Input, OnInit, Output } from '@angular/core'
 
 @Component({
   selector: 'app-button',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
+  @Input() text = ''
+  @Input() color = ''
+  //@Input() disabled = ''; ESTUDAR
+  //@Output() handleClick = ''; ESTUDAR
+
   constructor() {}
 
   ngOnInit(): void {}
