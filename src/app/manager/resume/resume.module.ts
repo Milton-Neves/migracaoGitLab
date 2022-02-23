bc85b597eac050e4d4e287771a8d85fbf5bc21b7
BiAaa <anny.ribeiro@parnamirim.rn.gov.br>
2022-02-23 10:31:44 -0300

    feat: criação do componente para listar curriculos arquivados

--- src/app/manager/resume/resume.module.ts
+++ src/app/manager/resume/resume.module.ts
@@ -5,9 +5,14 @@ import { ResumeRoutingModule } from './resume-routing.module'
 import { ResumeComponent } from './resume.component'
 import { ActiveResumeListComponent } from './components/active-resume-list/active-resume-list.component'
 import { SharedModule } from 'src/app/shared/shared.module'
+import { ArchivedResumeListComponent } from './components/archived-resume-list/archived-resume-list.component'
 
 @NgModule({
-  declarations: [ResumeComponent, ActiveResumeListComponent],
+  declarations: [
+    ResumeComponent,
+    ActiveResumeListComponent,
+    ArchivedResumeListComponent,
+  ],
   imports: [CommonModule, ResumeRoutingModule, SharedModule],
 })
 export class ResumeModule {}
