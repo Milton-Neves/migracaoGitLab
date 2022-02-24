c9d7e8d4d4a7aecf127080bd984a4e3b9a773b36
Milton Bezerra das  Neves Neto <milton@parnamirim.rn>
2022-02-24 11:57:16 -0300

    fit: implementando component

--- src/app/manager/forwarding/forwarding.module.ts
+++ src/app/manager/forwarding/forwarding.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common'
 import { ForwardingRoutingModule } from './forwarding-routing.module'
 import { ForwardingComponent } from './forwarding.component'
 import { SharedModule } from 'src/app/shared/shared.module'
+import { ForwardingCardComponent } from './components/forwarding-card/forwarding-card.component'
 
 @NgModule({
-  declarations: [ForwardingComponent],
+  declarations: [ForwardingComponent, ForwardingCardComponent],
   imports: [CommonModule, ForwardingRoutingModule, SharedModule],
 })
 export class ForwardingModule {}
