0d1ddae39e43debe8db8ab209d2a7ad489d7aed7
BiAaa <anny.ribeiro@parnamirim.rn.gov.br>
2022-02-24 12:25:21 -0300

    feat: criando componente

--- src/app/manager/company/company.module.ts
+++ src/app/manager/company/company.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common'
 import { CompanyRoutingModule } from './company-routing.module'
 import { CompanyComponent } from './company.component'
 import { SharedModule } from 'src/app/shared/shared.module'
+import { CompanyCardComponent } from './components/company-card/company-card.component'
 
 @NgModule({
-  declarations: [CompanyComponent],
+  declarations: [CompanyComponent, CompanyCardComponent],
   imports: [CommonModule, CompanyRoutingModule, SharedModule],
 })
 export class CompanyModule {}
