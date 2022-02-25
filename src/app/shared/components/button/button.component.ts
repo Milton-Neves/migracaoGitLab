f293bbeb7aa455fe6a0784914d03037b4e76de2d
Igor <igorj000@gmail.com>
2022-02-25 12:57:08 -0300

    refactor: refatorando componente de botao

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,20 +1,19 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
-  @Input() text?: string;
-  @Input() color!: string;
-  @Input() disabled: boolean = false;
-  @Output() handleClick = new EventEmitter();
+  @Input() text?: string
+  @Input() textColor?: string = '#FFFFFF'
+  @Input() color!: string
+  @Input() disabled: boolean = false
+  @Output() handleClick = new EventEmitter()
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onHandleClick($event: any) {
     this.handleClick.emit($event)
