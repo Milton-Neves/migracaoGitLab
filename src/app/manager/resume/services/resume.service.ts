0353512e40e2ad4ad3254ac5094124cfb3566472
Gabriel Duarte <gabriel.dantas@parnamirim.rn.gov.br>
2022-03-11 12:24:31 -0300

    feat: recebendo informações de curriculos do backend, criado paginação no front de curriculos

---
+++ src/app/manager/resume/services/resume.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { ApiResponse } from '@shared/models/api-response.model'
+import { environment } from 'environments/environment'
+import { Observable } from 'rxjs'
+
+import { ResumeProps } from '../entities/resume.model'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ResumeService {
+  endpoints = {
+    listAll: () => 'resume',
+    getOne: (id: number) => `resume/${id}`,
+  }
+
+  constructor(private http: HttpClient) {}
+
+  getResume(params?: any): Observable<ApiResponse<ResumeProps[]>> {
+    return this.http.get<ApiResponse<ResumeProps[]>>(
+      `${environment.baseUrl}/api/${this.endpoints.listAll()}`,
+      {
+        params: {
+          ...params,
+        },
+      }
+    )
+  }
+
+  getOneResume(id: number): Observable<ApiResponse<ResumeProps>> {
+    return this.http.get<ApiResponse<ResumeProps>>(
+      `${environment.baseUrl}/api/${this.endpoints.getOne(id)}`
+    )
+  }
+}
