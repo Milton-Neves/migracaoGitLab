852b2f141702e3ae25d63fcb133a20af0f255801
Vinicius <vinicius.01deagosto@gmail.com>
2022-03-11 08:38:40 -0300

    feat: add verificacao de rota ativa e inicio de colunas adicionais

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 import { ResumeViewComponent } from 'app/manager/resume/components/resume-view/resume-view.component'
 import { NgxModalService } from 'lib/ngx-modal/src/public-api'
 
@@ -8,11 +9,17 @@ import { NgxModalService } from 'lib/ngx-modal/src/public-api'
   styleUrls: ['./table.component.scss'],
 })
 export class TableComponent implements OnInit {
-  constructor(private modalService: NgxModalService) {}
+  routes: string[] = ['/gerenciador/curriculos', '/gerenciador/usuarios']
+  activedRoute!: string
+  @Input() additionalColumn?: string
+
+  constructor(private modalService: NgxModalService, private router: Router) {}
 
   viewResume() {
     let modal = this.modalService.open(ResumeViewComponent).subscribe()
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.activedRoute = this.router.url
+  }
 }
