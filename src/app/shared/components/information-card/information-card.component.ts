2fb53a7a3d2b6945b2c3e0037290681e1c691290
BiAaa <anny.ribeiro@parnamirim.rn.gov.br>
2022-03-16 12:40:38 -0300

    fix: coneção com o app information e mudanças na estilização

--- src/app/shared/components/information-card/information-card.component.ts
+++ src/app/shared/components/information-card/information-card.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core'
 export class InformationCardComponent implements OnInit {
   @Input() title!: string
   @Input() career!: string
-
+  @Input() stylesInformation!: any
   constructor() {}
 
   ngOnInit(): void {}
