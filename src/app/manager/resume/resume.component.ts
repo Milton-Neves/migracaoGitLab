3febaa938ec1424a9d88c661baaa06820a2c171d
Milton Bezerra das  Neves Neto <milton@parnamirim.rn>
2022-03-17 14:08:37 -0300

    feat: manipulando component

--- src/app/manager/resume/resume.component.ts
+++ src/app/manager/resume/resume.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./resume.component.scss'],
 })
 export class ResumeComponent implements OnInit {
+  currentTab!: string
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  changeCurrentTab(tabNav: string) {
+    this.currentTab = tabNav
+    console.log(this.currentTab)
+  }
 }
