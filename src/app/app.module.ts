970ade23d70729f428467cda4b94f983f687758c
Igor <igorj000@gmail.com>
2022-04-27 08:35:03 -0300

    feat: criando interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { SharedModule } from '@shared/shared.module'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { ToastrModule } from 'ngx-toastr'
 
+import { ErrorInterceptor } from '@core/interceptors/error-interceptor'
+import { JwtInterceptor } from '@core/interceptors/jwt-interceptor'
+import { SharedModule } from '@shared/shared.module'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 
@@ -17,7 +20,14 @@ import { AppComponent } from './app.component'
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true,
+    },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
