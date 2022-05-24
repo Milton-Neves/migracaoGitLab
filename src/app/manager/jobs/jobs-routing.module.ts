3518c36b9acea4887644781b0c9fa20d08f7c584
BiAaa <anny.ribeiro@parnamirim.rn.gov.br>
2022-05-24 09:19:27 -0300

    Merge branch 'develop' of https://gitlab.com/gctiparnamirim/banco-empregos/sgi-banco-de-empregos into feat/#2243

--- src/app/manager/jobs/jobs-routing.module.ts
+++ src/app/manager/jobs/jobs-routing.module.ts
@@ -1,3 +1,4 @@
+import { JobsRegistrationComponent } from './components/jobs-registration/jobs-registration.component'
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { JobsComponent } from './jobs.component'
@@ -6,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: JobsComponent,
+  },
+  {
+    path: 'cadastrar',
+    component: JobsRegistrationComponent,
   },
 ]
 
