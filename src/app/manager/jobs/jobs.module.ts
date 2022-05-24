d225fbcd3141ced07e028bba9223755dc4bc105f
Milton Bezerra das  Neves Neto <milton@parnamirim.rn>
2022-05-24 14:59:21 -0300

    feat: component implementado

--- src/app/manager/jobs/jobs.module.ts
+++ src/app/manager/jobs/jobs.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common'
 
 import { JobsRoutingModule } from './jobs-routing.module'
 import { JobsComponent } from './jobs.component'
+import { JobsEditComponent } from './components/jobs-edit/jobs-edit.component'
+import { SharedModule } from '@shared/shared.module'
 
 @NgModule({
-  declarations: [JobsComponent],
-  imports: [CommonModule, JobsRoutingModule],
+  declarations: [JobsComponent, JobsEditComponent],
+  imports: [CommonModule, JobsRoutingModule, SharedModule],
 })
 export class JobsModule {}
