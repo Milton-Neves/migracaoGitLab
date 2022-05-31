dc02f708971027f606e03ff285c8fd2503a211b2
Laura <laura@GCTI-07.parnamirim.rn>
2022-05-31 08:51:28 -0300

    feat: componente para listar os cargos finalizado

--- src/app/manager/jobs/components/jobs-list/jobs-list.component.ts
+++ src/app/manager/jobs/components/jobs-list/jobs-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { Job } from '@core/interfaces/resume/job'
+import { Observable, of } from 'rxjs'
+import { map, tap } from 'rxjs/operators'
+import { ResumeService } from 'app/manager/resume/services/resume.service'
+import { PaginationService } from '@shared/services/pagination.service'
+import { JobService } from '../../services/job.service'
+import { ApiResponse } from '@core/interfaces/api-response.model'
 
 @Component({
   selector: 'app-jobs-list',
@@ -6,7 +13,53 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./jobs-list.component.scss'],
 })
 export class JobsListComponent implements OnInit {
-  constructor() {}
+  @Input() stylesInformation: any
+  // jobs: Job[] = []
+  jobs!: any[]
+  totalCountJobs: number = 0
+  // pagination$?: Observable<any>
+  jobs$!: Observable<Job[]>
 
-  ngOnInit(): void {}
+  constructor(
+    private jobService: JobService,
+    private paginationService: PaginationService
+  ) {}
+
+  ngOnInit(): void {
+    this.getJobsFromServer()
+    // this.getJobList()
+  }
+
+  // getJobList() {
+  //   this.jobService
+  //     .findAll()
+  //     .subscribe((job: ApiResponse<Job[]>) => {
+  //       this.jobs = job.data
+  //       console.log(job.data)
+  //     })
+  // }
+
+  getJobsFromServer(page: number = 1, params?: any) {
+    this.jobService
+      .findAll('', { statusJob: true })
+      .pipe(
+        tap((jobs) => {
+          this.totalCountJobs = jobs.data.length
+          this.paginateJobs(page, jobs.data)
+        }),
+        map((res) => res.data)
+      )
+      .subscribe()
+  }
+
+  paginateJobs(page: number, jobs: any[]) {
+    let { results, pagination } = this.paginationService.createPagination(
+      page,
+      jobs,
+      this.paginationService.verifyPageSize()
+    )
+
+    this.jobs = results
+    this.jobs$ = of(pagination)
+  }
 }
