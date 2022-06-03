698aa98944b1d78f3c8523355c01e92c43d9b427
Igor José <igorj000@gmail.com>
2022-06-03 13:37:01 -0300

    feat: [WIP] implementando cadastro de empresa

--- src/app/manager/company/company.module.ts
+++ src/app/manager/company/company.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { ReactiveFormsModule } from '@angular/forms'
 import { SharedModule } from '@shared/shared.module'
 import { NgxMaskModule } from 'ngx-mask'
 
@@ -26,6 +27,7 @@ import { CompanyViewComponent } from './components/company-view/company-view.com
     CommonModule,
     CompanyRoutingModule,
     SharedModule,
+    ReactiveFormsModule,
     NgxMaskModule.forChild(),
   ],
 })
--- src/app/manager/company/components/company-registration/company-registration.component.ts
+++ src/app/manager/company/components/company-registration/company-registration.component.ts
@@ -1,12 +1,50 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup } from '@angular/forms'
+import { EMPTY, Subscription } from 'rxjs'
+import { catchError, map } from 'rxjs/operators'
+import { Company } from '../../entities/company.model'
+import { CompanyService } from '../../services/company.service'
 
 @Component({
   selector: 'app-company-registration',
   templateUrl: './company-registration.component.html',
   styleUrls: ['./company-registration.component.scss'],
 })
-export class CompanyRegistrationComponent implements OnInit {
-  constructor() {}
+export class CompanyRegistrationComponent implements OnInit, OnDestroy {
+  form!: FormGroup
+  serviceubscription = new Subscription()
 
-  ngOnInit(): void {}
+  constructor(
+    private builder: FormBuilder,
+    private companyService: CompanyService
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.createForm()
+  }
+
+  onSubmit() {
+    const values = this.form.value as Company
+
+    this.serviceubscription = this.companyService
+      .create(values)
+      .pipe(
+        map((res) => res),
+        catchError(() => EMPTY)
+      )
+      .subscribe()
+  }
+
+  private createForm(): FormGroup {
+    return this.builder.group({
+      name: [''],
+      cnpj: [''],
+      companyName: [],
+      amountEmployees: [],
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.serviceubscription.unsubscribe()
+  }
 }
