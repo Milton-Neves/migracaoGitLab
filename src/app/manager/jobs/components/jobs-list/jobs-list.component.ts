3a0bc6bdf95c4f9a2141887eac85a4de15edc80b
Laura <laura@GCTI-07.parnamirim.rn>
2022-06-06 12:36:30 -0300

    feat: funcionalidade para listar cargos

--- src/app/manager/jobs/components/jobs-list/jobs-list.component.ts
+++ src/app/manager/jobs/components/jobs-list/jobs-list.component.ts
@@ -1,10 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core'
-import { Job } from '@core/interfaces/resume/job'
+import { Component, OnInit } from '@angular/core'
 import { PaginationService } from '@shared/services/pagination.service'
 import { Observable, of } from 'rxjs'
 import { map, tap } from 'rxjs/operators'
 
-import { JobService } from '../../services/job.service'
+import { WorkfieldService } from '@shared/services/workfield.service'
 
 @Component({
   selector: 'app-jobs-list',
@@ -12,14 +11,14 @@ import { JobService } from '../../services/job.service'
   styleUrls: ['./jobs-list.component.scss'],
 })
 export class JobsListComponent implements OnInit {
-  @Input() stylesInformation: any
-  jobs: any[] = []
+  workfield: any[] = []
+  tempJobsList: any[] = []
   totalCountJobs: number = 0
-  jobs$!: Observable<Job[]>
+  pagination$?: Observable<any>
 
   constructor(
-    private jobService: JobService,
-    private paginationService: PaginationService
+    private paginationService: PaginationService,
+    private workfieldService: WorkfieldService
   ) {}
 
   ngOnInit(): void {
@@ -27,27 +26,39 @@ export class JobsListComponent implements OnInit {
   }
 
   getJobsFromServer(page: number = 1, params?: any) {
-    this.jobService
-      .findAll('', {})
+    this.workfieldService
+      .findAll()
       .pipe(
-        tap((jobs) => {
-          this.totalCountJobs = jobs.data.length
-
-          this.paginateJobs(page, jobs.data)
+        tap((workfields: any) => {
+          this.totalCountJobs = 0
+          this.tempJobsList = []
+          workfields.data.forEach((position: any) => {
+            this.tempJobsList = this.tempJobsList.concat(
+              position.jobs.map((value: any) => {
+                return {
+                  name: value.name,
+                  color: position.colorCode,
+                  workfield: position.name,
+                }
+              })
+            )
+            this.totalCountJobs = this.tempJobsList.length
+          })
+          this.paginateJobs(page, this.tempJobsList)
         }),
         map((res) => res.data)
       )
       .subscribe()
   }
 
-  paginateJobs(page: number, jobs: any[]) {
+  paginateJobs(page: number, workfield: any[]) {
     let { results, pagination } = this.paginationService.createPagination(
       page,
-      jobs,
+      workfield,
       this.paginationService.verifyPageSize()
     )
 
-    this.jobs = results
-    this.jobs$ = of(pagination)
+    this.workfield = results
+    this.pagination$ = of(pagination)
   }
 }
