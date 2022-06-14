dbe81dd4ccc4fcc170be0d4ad70c0f22a2911108
Igor José <igorj000@gmail.com>
2022-06-14 10:55:26 -0300

    feat: [WIP] implementando cadastro de empresa

--- src/app/manager/company/components/company-registration/company-registration.component.ts
+++ src/app/manager/company/components/company-registration/company-registration.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core'
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms'
+
+import { EMPTY, Subscription } from 'rxjs'
+import { catchError, map, tap } from 'rxjs/operators'
 import { NgxViacepService } from '@brunoc/ngx-viacep'
+
 import { Workfield } from '@core/interfaces/resume/workfield'
 import { EnumService } from '@shared/services/enum.service'
 import { WorkfieldService } from '@shared/services/workfield.service'
-import { BehaviorSubject, EMPTY, Subscription } from 'rxjs'
-import { catchError, map, tap } from 'rxjs/operators'
 import { Company } from '../../entities/company.model'
 import { CompanyService } from '../../services/company.service'
 
@@ -16,9 +18,13 @@ import { CompanyService } from '../../services/company.service'
 })
 export class CompanyRegistrationComponent implements OnInit, OnDestroy {
   form!: FormGroup
-  serviceubscription = new Subscription()
   amountEmployees: string[] = []
   workfields: Workfield[] = []
+
+  serviceSubscription = new Subscription()
+  getWorkfieldsSubscription = new Subscription()
+  getAmountEmployeesSubscription = new Subscription()
+  zipCodeSubscription = new Subscription()
 
   constructor(
     private builder: FormBuilder,
@@ -30,31 +36,25 @@ export class CompanyRegistrationComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.form = this.createForm()
-    this.getWorkfields()
-    this.getAmountEmployees()
+    this.getWorkfieldsSubscription = this.getWorkfields().subscribe()
+    this.getAmountEmployeesSubscription = this.getAmountEmployees().subscribe()
   }
 
   getAmountEmployees() {
-    return this.enumService
-      .getAmountEmployees()
-      .pipe(
-        map((response: string[]) => {
-          this.amountEmployees = response
-        })
-      )
-      .subscribe()
+    return this.enumService.getAmountEmployees().pipe(
+      map((response: string[]) => {
+        this.amountEmployees = response
+      })
+    )
   }
 
   getWorkfields() {
-    return this.workfieldService
-      .findAll()
-      .pipe(
-        map(({ data }) => data),
-        map((workfields: Workfield[]) => {
-          this.workfields = workfields
-        })
-      )
-      .subscribe()
+    return this.workfieldService.findAll().pipe(
+      map(({ data }) => data),
+      map((workfields: Workfield[]) => {
+        this.workfields = workfields
+      })
+    )
   }
 
   onSubmit() {
@@ -63,7 +63,7 @@ export class CompanyRegistrationComponent implements OnInit, OnDestroy {
       workfield: JSON.parse(this.form.get('workfield')?.value),
     } as Company
 
-    this.serviceubscription = this.companyService
+    this.serviceSubscription = this.companyService
       .create(values)
       .pipe(
         map((res) => res),
@@ -74,12 +74,18 @@ export class CompanyRegistrationComponent implements OnInit, OnDestroy {
 
   private createForm(): FormGroup {
     return this.builder.group({
-      name: this.builder.control('', [Validators.required]),
-      cnpj: this.builder.control('', [Validators.required]),
+      name: this.builder.control('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      cnpj: this.builder.control('', [
+        Validators.required,
+        Validators.maxLength(16),
+      ]),
       companyName: this.builder.control('', [Validators.required]),
-      amountEmployees: this.builder.control(null, [Validators.required]),
+      amountEmployees: this.builder.control('', [Validators.required]),
       workfield: this.builder.control('', [Validators.required]),
-      email: this.builder.control('', [Validators.required]),
+      email: this.builder.control('', [Validators.required, Validators.email]),
       phoneNumbers: this.builder.array([
         this.builder.group({
           number: this.builder.control('', [Validators.required]),
@@ -98,7 +104,10 @@ export class CompanyRegistrationComponent implements OnInit, OnDestroy {
       legalRepresentative: this.builder.group({
         name: this.builder.control('', [Validators.required]),
         cellNumber: this.builder.control('', [Validators.required]),
-        email: this.builder.control('', [Validators.required]),
+        email: this.builder.control('', [
+          Validators.required,
+          Validators.email,
+        ]),
         phoneNumber: this.builder.control('', [Validators.required]),
       }),
     })
@@ -113,7 +122,7 @@ export class CompanyRegistrationComponent implements OnInit, OnDestroy {
       if (cepIsValid.test(cep)) {
         this.cleanAddressForm()
 
-        this.viacep
+        this.zipCodeSubscription = this.viacep
           .buscarPorCep(cep)
           .pipe(tap((address) => this.fillAddress(address)))
           .subscribe()
@@ -139,11 +148,31 @@ export class CompanyRegistrationComponent implements OnInit, OnDestroy {
     })
   }
 
+  addNewPhoneNumber(): void {
+    this.phoneNumbers.push(
+      this.builder.group({
+        number: this.builder.control(''),
+        isOwner: this.builder.control(false),
+      })
+    )
+  }
+
+  removePhoneNumber(index: number): void {
+    this.phoneNumbers.removeAt(index)
+  }
+
+  get phoneNumbers(): FormArray {
+    return this.form.get('phoneNumbers') as FormArray
+  }
+
   get address(): FormGroup {
     return this.form.get('address') as FormGroup
   }
 
   ngOnDestroy(): void {
-    this.serviceubscription.unsubscribe()
+    this.serviceSubscription.unsubscribe()
+    this.getWorkfieldsSubscription.unsubscribe()
+    this.getAmountEmployeesSubscription.unsubscribe()
+    this.zipCodeSubscription.unsubscribe()
   }
 }
