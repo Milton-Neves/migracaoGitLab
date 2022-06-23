2dc91d14c6bb27ca5df7d8ef21e4aaa2092b7500
Igor José <igorj000@gmail.com>
2022-06-23 15:26:25 -0300

    refactor: componetizando os modulos

--- src/app/manager/modules/modules.module.ts
+++ src/app/manager/modules/modules.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common'
 import { ModulesRoutingModule } from './modules-routing.module'
 import { ModulesComponent } from './modules.component'
 import { SharedModule } from '@shared/shared.module'
+import { ModuleComponent } from './components/module/module.component'
 
 @NgModule({
-  declarations: [ModulesComponent],
+  declarations: [ModulesComponent, ModuleComponent],
   imports: [CommonModule, ModulesRoutingModule, SharedModule],
 })
 export class ModulesModule {}
