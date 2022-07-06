23d2fb416b12b0b3dbba8a31874e2746c2004b31
Vinicius <vinicius.01deagosto@gmail.com>
2022-07-06 08:36:06 -0300

    fix: verifações para situação de contratado e contratado_fora

--- src/app/manager/resume/components/archiving-modal/archiving-modal.component.ts
+++ src/app/manager/resume/components/archiving-modal/archiving-modal.component.ts
@@ -2,12 +2,12 @@ import { Component, OnInit } from '@angular/core'
 import { Archiving } from '@core/interfaces/resume/archiving'
 import { Resume } from '@core/interfaces/resume/resume'
 import { EnumService } from '@shared/services/enum.service'
+import { LegalUserService } from 'app/manager/company/services/legal-user.service'
 import { NgxModalService } from 'lib/ngx-modal/src/public-api'
 import { iif, Observable, of } from 'rxjs'
 import { map, mergeMap, tap } from 'rxjs/operators'
 
 import { ResumeService } from '../../services/resume.service'
-import { CompanyService } from './../../../company/services/company.service'
 
 @Component({
   selector: 'app-archiving-modal',
@@ -19,12 +19,12 @@ export class ArchivingModalComponent implements OnInit {
   motivesArchiving$!: Observable<String[]>
   companies$!: Observable<any[]>
   archivingResume!: Archiving
-  searchCompanyName!: string
+  searchCompanyName: string = ''
   focus: boolean = false
   constructor(
     private modalService: NgxModalService,
     private enumService: EnumService,
-    private companyService: CompanyService,
+    private legalUserService: LegalUserService,
     private resumeService: ResumeService
   ) {}
 
@@ -43,10 +43,10 @@ export class ArchivingModalComponent implements OnInit {
     this.motivesArchiving$ = this.enumService
       .findAll('motive_archiving')
       .pipe(map((res: any) => res as String[]))
-    this.companyService
-      .findAll()
+    this.legalUserService
+      .findAll('companies')
       .pipe(
-        map((res: any) => res.data.content),
+        map((res: any) => res.data),
         tap((res) => {
           this.companies$ = of(res)
         })
@@ -64,14 +64,14 @@ export class ArchivingModalComponent implements OnInit {
   autoComplete() {
     this.companies$ = this.companies$.pipe(
       map((res: any) =>
-        res.filter((company: { name: string }) =>
-          company.name.toLowerCase().match(this.searchCompanyName.toLowerCase())
+        res.filter((company: string) =>
+          company.toLowerCase().match(this.searchCompanyName.toLowerCase())
         )
       ),
       mergeMap((listCompanies) =>
         iif(
           () => listCompanies.length <= 0,
-          of([{ name: 'Nenhum empresa encontrada', disabled: true }]),
+          of(['Nenhuma empresa encontrada']),
           of(listCompanies)
         )
       )
@@ -81,7 +81,7 @@ export class ArchivingModalComponent implements OnInit {
   handleForm() {
     if (this.archivingResume.archiving.motive == null) return true
     if (
-      this.archivingResume.archiving.motive == 'CONTRATADO' &&
+      this.archivingResume.archiving.motive == 'Contratado(a)' &&
       this.archivingResume.companyName == null
     )
       return true
@@ -89,16 +89,16 @@ export class ArchivingModalComponent implements OnInit {
   }
 
   setValueToCompanyName(company: any) {
-    if (!company.disabled) {
-      this.searchCompanyName = company.name
-      this.archivingResume.companyName = company.name
+    if (company != 'Nenhuma empresa encontrada') {
+      this.searchCompanyName = company
+      this.archivingResume.companyName = company
     } else this.archivingResume.companyName = null
   }
 
   changeStateFocus() {
     setTimeout(() => {
       this.focus = !this.focus
-      if (this.searchCompanyName != undefined) this.autoComplete()
+      this.autoComplete()
     }, 100)
   }
 }
